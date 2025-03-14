4eb0d261e7c112a4056bb36c17c6be62f413a328
MaximoGismondi <maximogismondi@gmail.com>
2025-03-14 11:01:10 -0300

    Fix: Price is f64

--- ejercicios/rest-workshop/ej2/src/model/model.go
+++ ejercicios/rest-workshop/ej2/src/model/model.go
@@ -1,9 +1,9 @@
 package model
 
 type Item struct {
-	Name  string `json:"name" binding:"required"`
-	ID    string `json:"id" binding:"required"`
-	Price int    `json:"price" binding:"required"`
+	Name  string  `json:"name" binding:"required"`
+	ID    string  `json:"id" binding:"required"`
+	Price float64 `json:"price" binding:"required"`
 }
 
 type Error struct {
